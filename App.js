00aa52d762543b5319671100e134fc68d1693d41
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2024-07-28 12:55:53 +0300

    screen2

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Screen1 from './Components/screen1';
 import Screen3 from './Components/screen3';
 import Screen4 from './Components/screen4';
+import Screen2 from './Components/screen2';
 import RNFS from 'react-native-fs';
 import { persistor,store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,7 +13,7 @@ import { Provider } from 'react-redux';
 
 const Tab = createBottomTabNavigator();
 
-const Screen2 = ({navigation})=>{
+const Scren2 = ({navigation})=>{
     return(
         <View style={style.container}>
             <Text style={style.text}>Screen two</Text>
